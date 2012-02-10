01aa7a698fda857d3a8ab5688c297f9a3598824d
Shai Almog <shai@codenameone.com>
2012-02-10 14:47:27 +0000

    Blackberry skin

--- Skins/build_skins.sh
+++ Skins/build_skins.sh
@@ -2,23 +2,27 @@
 
 cd android
 cp -f ../../Themes/androidTheme.res .
-zip ../android.skin *
+zip -0 ../android.skin *
 
 cd ../feature_phone
-zip ../feature_phone.skin *
+zip -0 ../feature_phone.skin *
 
 cd ../ipad
 cp -f ../../Themes/iPhoneTheme.res .
-zip ../ipad.skin *
+zip -0 ../ipad.skin *
 
 cd ../iphone4
 cp -f ../../Themes/iPhoneTheme.res .
-zip ../iphone4.skin *
+zip -0 ../iphone4.skin *
 
 cd ../nexus
 cp -f ../../Themes/androidTheme.res .
-zip ../nexus.skin *
+zip -0 ../nexus.skin *
 
 cd ../iphone3gs
 cp -f ../../Themes/iPhoneTheme.res .
-zip ../iphone3gs.skin *
+zip -0 ../iphone3gs.skin *
+
+cd ../torch
+cp -f ../../Themes/blackberry_theme.res .
+zip -0 ../torch.skin *
