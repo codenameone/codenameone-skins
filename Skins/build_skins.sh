f80374a67607ee1d831fdb245f1230b20ba696a3
Shai Almog <shai@codenameone.com>
2013-11-16 08:26:07 +0000

    Updating skins again

--- Skins/build_skins.sh
+++ Skins/build_skins.sh
@@ -28,6 +28,27 @@ cp -f ../../Themes/iOS7Theme.res .
 zip -0 ../iphone5_os7.skin *
 cp -f ../iphone5_os7.skin ../OTA/iphone5_os7.skin
 
+cd ../iphone3gs_os7
+cp -f ../../Themes/iOS7Theme.res .
+zip -0 ../iphone3gs_os7.skin *
+cp -f ../iphone3gs_os7.skin ../OTA/iphone3gs_os7.skin
+
+cd ../ipad_os7
+cp -f ../../Themes/iOS7Theme.res .
+zip -0 ../ipad_os7.skin *
+cp -f ../ipad_os7.skin ../OTA/ipad_os7.skin
+
+cd ../ipad3_os7
+cp -f ../../Themes/iOS7Theme.res .
+zip -0 ../ipad3_os7.skin *
+cp -f ../ipad3_os7.skin ../OTA/ipad3_os7.skin
+
+cd ../iphone4_os7
+cp -f ../../Themes/iOS7Theme.res .
+zip -0 ../iphone4_os7.skin *
+cp -f ../iphone4_os7.skin ../OTA/iphone4_os7.skin
+
+
 cd ../nexus
 cp -f ../../Themes/androidTheme.res .
 zip -0 ../nexus.skin *
@@ -47,3 +68,11 @@ zip -0 ../xoom.skin *
 cd ../lumia
 cp -f ../../Themes/winTheme.res .
 zip -0 ../lumia.skin *
+
+cd ../OTA/HTCOne
+cp -f ../../../Themes/android_holo_light.res .
+zip -0 ../HTCOne.skin *
+
+cd ../NokiaLumia920
+cp -f ../../../Themes/android_holo_light.res .
+zip -0 ../NokiaLumia920.skin *
