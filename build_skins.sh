1d70d45c350891eeb5638c884d729c3eeeb698aa
Shai Almog <shai@codenameone.com>
2018-02-04 07:36:42 +0200

    Added new skins that are far more consistent with everything. This will be deployed Friday as we need the new non-rectangular skin support in the latest update

--- build_skins.sh
+++ build_skins.sh
@@ -107,5 +107,174 @@ cd ../K7
 cp -f ../../cn1/Themes/android_holo_light.res .
 zip ../K7.skin *
 
+cd ../Phones
+
+cd GooglePixel
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../GooglePixel.skin *
+cd ..
+                        
+cd GooglePixel2
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../GooglePixel2.skin *
+cd ..
+                        
+cd GooglePixel2XL
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../GooglePixel2XL.skin *
+cd ..
+                        
+cd HTCOneA9
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../HTCOneA9.skin *
+cd ..
+                        
+cd HTCOneM8
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../HTCOneM8.skin *
+cd ..
+                        
+cd HuaweiP8
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../HuaweiP8.skin *
+cd ..
+                        
+cd MicrosoftLumia950
+cp -f ../../../cn1/Themes/winTheme.res .
+zip ../../MicrosoftLumia950.skin *
+cd ..
+                        
+cd MotoE
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../MotoE.skin *
+cd ..
+                        
+cd MotoG
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../MotoG.skin *
+cd ..
+                        
+cd Nexus4
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../Nexus4.skin *
+cd ..
+                        
+cd Nexus5X
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../Nexus5X.skin *
+cd ..
+                        
+cd Nexus6P
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../Nexus6P.skin *
+cd ..
+                        
+cd SamsungGalaxyGrandPrime
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../SamsungGalaxyGrandPrime.skin *
+cd ..
+                        
+cd SamsungGalaxyNote5
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../SamsungGalaxyNote5.skin *
+cd ..
+                        
+cd SamsungGalaxyS3
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../SamsungGalaxyS3.skin *
+cd ..
+                        
+cd SamsungGalaxyS5
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../SamsungGalaxyS5.skin *
+cd ..
+                        
+cd SamsungGalaxyS7
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../SamsungGalaxyS7.skin *
+cd ..
+                        
+cd SamsungGalaxyS8
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../SamsungGalaxyS8.skin *
+cd ..
+                        
+cd iPhone5c
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone5c.skin *
+cd ..
+                        
+cd iPhone5s
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone5s.skin *
+cd ..
+                        
+cd iPhone6s
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone6s.skin *
+cd ..
+                        
+cd iPhone6sPlus
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone6sPlus.skin *
+cd ..
+                        
+cd iPhone7
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone7.skin *
+cd ..
+                        
+cd iPhone7Plus
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone7Plus.skin *
+cd ..
+                        
+cd iPhone8
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone8.skin *
+cd ..
+                        
+cd iPhone8Plus
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhone8Plus.skin *
+cd ..
+                        
+cd iPhoneX
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPhoneX.skin *
+cd ..
+
+cd ../Tablets
+
+cd MicrosoftSurface3
+cp -f ../../../cn1/Themes/winTheme.res .
+zip ../../MicrosoftSurface3.skin *
+cd ..
+
+cd MicrosoftSurfacePro4
+cp -f ../../../cn1/Themes/winTheme.res .
+zip ../../MicrosoftSurfacePro4.skin *
+cd ..
+                        
+cd Nexus9
+cp -f ../../../cn1/Themes/android_holo_light.res .
+zip ../../Nexus9.skin *
+cd ..
+
+cd iPadAir2
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPadAir2.skin *
+cd ..
+                        
+cd iPadMini4
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPadMini4.skin *
+cd ..
+                        
+cd iPadPro
+cp -f ../../../cn1/Themes/iOS7Theme.res .
+zip ../../iPadPro.skin *
+cd ..
+                        
 cd ..
 cp *.skin OTA
