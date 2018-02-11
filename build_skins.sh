6047b3c5061a37e5f16d6f2f8d258ffdef77fe74
Shai Almog <shai@codenameone.com>
2018-02-11 22:36:00 +0200

    Removed some skin files to reduce the repo size

--- build_skins.sh
+++ build_skins.sh
@@ -2,279 +2,279 @@
 
 cd android
 cp -f ../../cn1/Themes/androidTheme.res .
-zip ../android.skin *
+zip -9 ../OTA/android.skin *
 
 cd ../feature_phone
-zip ../feature_phone.skin *
+zip -9 ../OTA/feature_phone.skin *
 
 cd ../ipad
 cp -f ../../cn1/Themes/iPhoneTheme.res .
-zip ../ipad.skin *
+zip -9 ../OTA/ipad.skin *
 
 cd ../ipad3
 cp -f ../../cn1/Themes/iPhoneTheme.res .
-zip ../ipad3.skin *
+zip -9 ../OTA/ipad3.skin *
 
 cd ../iphone4
 cp -f ../../cn1/Themes/iPhoneTheme.res .
-zip ../iphone4.skin *
+zip -9 ../OTA/iphone4.skin *
 
 cd ../iphone5
 cp -f ../../cn1/Themes/iPhoneTheme.res .
-zip ../iphone5.skin *
+zip -9 ../OTA/iphone5.skin *
 
 cd ../iphone5_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone5_os7.skin *
+zip -9 ../OTA/iphone5_os7.skin *
 
 cd ../iphone3gs_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone3gs_os7.skin *
+zip -9 ../OTA/iphone3gs_os7.skin *
 
 cd ../ipad_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../ipad_os7.skin *
+zip -9 ../OTA/ipad_os7.skin *
 
 cd ../ipad3_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../ipad3_os7.skin *
+zip -9 ../OTA/ipad3_os7.skin *
 
 cd ../iphone4_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone4_os7.skin *
+zip -9 ../OTA/iphone4_os7.skin *
 
 cd ../iphone6Plus
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone6Plus.skin *
+zip -9 ../OTA/iphone6Plus.skin *
 
 cd ../iphone6
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone6.skin *
+zip -9 ../OTA/iphone6.skin *
 
 cd ../iphone5_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone5_os7.skin *
+zip -9 ../OTA/iphone5_os7.skin *
 
 cd ../iphone4_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../iphone4_os7.skin *
+zip -9 ../OTA/iphone4_os7.skin *
 
 cd ../ipad3_os7
 cp -f ../../cn1/Themes/iOS7Theme.res .
-zip ../ipad3_os7.skin *
+zip -9 ../OTA/ipad3_os7.skin *
 
 cd ../nexus
 cp -f ../../cn1/Themes/androidTheme.res .
-zip ../nexus.skin *
+zip -9 ../OTA/nexus.skin *
 
 cd ../iphone3gs
 cp -f ../../cn1/Themes/iPhoneTheme.res .
-zip ../iphone3gs.skin *
+zip -9 ../OTA/iphone3gs.skin *
 
 cd ../torch
 cp -f ../../cn1/Themes/blackberry_theme.res .
-zip ../torch.skin *
+zip -9 ../OTA/torch.skin *
 
 cd ../lumia
 cp -f ../../cn1/Themes/winTheme.res .
-zip ../lumia.skin *
+zip -9 ../OTA/lumia.skin *
 
 cd ../xoom
 cp -f ../../cn1/Themes/android_holo_light.res .
-zip ../xoom.skin *
+zip -9 ../OTA/xoom.skin *
 
 cd ../HTCOne
 cp -f ../../cn1/Themes/android_holo_light.res .
-zip ../HTCOne.skin *
+zip -9 ../OTA/HTCOne.skin *
 
 cd ../SamsungS7
 cp -f ../../cn1/Themes/android_holo_light.res .
-zip ../SamsungS7.skin *
+zip -9 ../OTA/SamsungS7.skin *
 
 cd ../HTC10
 cp -f ../../cn1/Themes/android_holo_light.res .
-zip ../HTC10.skin *
+zip -9 ../OTA/HTC10.skin *
 
 cd ../Nexus5
 cp -f ../../cn1/Themes/android_holo_light.res .
-zip ../Nexus5.skin *
+zip -9 ../OTA/Nexus5.skin *
 
 cd ../NokiaLumia920
 cp -f ../../cn1/Themes/winTheme.res .
-zip ../NokiaLumia920.skin *
+zip -9 ../OTA/NokiaLumia920.skin *
 
 cd ../K7
 cp -f ../../cn1/Themes/android_holo_light.res .
-zip ../K7.skin *
+zip -9 ../OTA/K7.skin *
 
 cd ../Phones
 
 cd GooglePixel
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../GooglePixel.skin *
+zip -9 ../../OTA/GooglePixel.skin *
 cd ..
                         
 cd GooglePixel2
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../GooglePixel2.skin *
+zip -9 ../../OTA/GooglePixel2.skin *
 cd ..
                         
 cd GooglePixel2XL
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../GooglePixel2XL.skin *
+zip -9 ../../OTA/GooglePixel2XL.skin *
 cd ..
                         
 cd HTCOneA9
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../HTCOneA9.skin *
+zip -9 ../../OTA/HTCOneA9.skin *
 cd ..
                         
 cd HTCOneM8
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../HTCOneM8.skin *
+zip -9 ../../OTA/HTCOneM8.skin *
 cd ..
                         
 cd HuaweiP8
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../HuaweiP8.skin *
+zip -9 ../../OTA/HuaweiP8.skin *
 cd ..
                         
 cd MicrosoftLumia950
 cp -f ../../../cn1/Themes/winTheme.res .
-zip ../../MicrosoftLumia950.skin *
+zip -9 ../../OTA/MicrosoftLumia950.skin *
 cd ..
                         
 cd MotoE
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../MotoE.skin *
+zip -9 ../../OTA/MotoE.skin *
 cd ..
                         
 cd MotoG
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../MotoG.skin *
+zip -9 ../../OTA/MotoG.skin *
 cd ..
                         
 cd Nexus4
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../Nexus4.skin *
+zip -9 ../../OTA/Nexus4.skin *
 cd ..
                         
 cd Nexus5X
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../Nexus5X.skin *
+zip -9 ../../OTA/Nexus5X.skin *
 cd ..
                         
 cd Nexus6P
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../Nexus6P.skin *
+zip -9 ../../OTA/Nexus6P.skin *
 cd ..
                         
 cd SamsungGalaxyGrandPrime
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../SamsungGalaxyGrandPrime.skin *
+zip -9 ../../OTA/SamsungGalaxyGrandPrime.skin *
 cd ..
                         
 cd SamsungGalaxyNote5
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../SamsungGalaxyNote5.skin *
+zip -9 ../../OTA/SamsungGalaxyNote5.skin *
 cd ..
                         
 cd SamsungGalaxyS3
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../SamsungGalaxyS3.skin *
+zip -9 ../../OTA/SamsungGalaxyS3.skin *
 cd ..
                         
 cd SamsungGalaxyS5
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../SamsungGalaxyS5.skin *
+zip -9 ../../OTA/SamsungGalaxyS5.skin *
 cd ..
                         
 cd SamsungGalaxyS7
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../SamsungGalaxyS7.skin *
+zip -9 ../../OTA/SamsungGalaxyS7.skin *
 cd ..
                         
 cd SamsungGalaxyS8
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../SamsungGalaxyS8.skin *
+zip -9 ../../OTA/SamsungGalaxyS8.skin *
 cd ..
                         
 cd iPhone5c
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone5c.skin *
+zip -9 ../../OTA/iPhone5c.skin *
 cd ..
                         
 cd iPhone5s
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone5s.skin *
+zip -9 ../../OTA/iPhone5s.skin *
 cd ..
                         
 cd iPhone6s
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone6s.skin *
+zip -9 ../../OTA/iPhone6s.skin *
 cd ..
                         
 cd iPhone6sPlus
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone6sPlus.skin *
+zip -9 ../../OTA/iPhone6sPlus.skin *
 cd ..
                         
 cd iPhone7
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone7.skin *
+zip -9 ../../OTA/iPhone7.skin *
 cd ..
                         
 cd iPhone7Plus
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone7Plus.skin *
+zip -9 ../../OTA/iPhone7Plus.skin *
 cd ..
                         
 cd iPhone8
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone8.skin *
+zip -9 ../../OTA/iPhone8.skin *
 cd ..
                         
 cd iPhone8Plus
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhone8Plus.skin *
+zip -9 ../../OTA/iPhone8Plus.skin *
 cd ..
                         
 cd iPhoneX
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPhoneX.skin *
+zip -9 ../../OTA/iPhoneX.skin *
 cd ..
 
 cd ../Tablets
 
 cd MicrosoftSurface3
 cp -f ../../../cn1/Themes/winTheme.res .
-zip ../../MicrosoftSurface3.skin *
+zip -9 ../../OTA/MicrosoftSurface3.skin *
 cd ..
 
 cd MicrosoftSurfacePro4
 cp -f ../../../cn1/Themes/winTheme.res .
-zip ../../MicrosoftSurfacePro4.skin *
+zip -9 ../../OTA/MicrosoftSurfacePro4.skin *
 cd ..
                         
 cd Nexus9
 cp -f ../../../cn1/Themes/android_holo_light.res .
-zip ../../Nexus9.skin *
+zip -9 ../../OTA/Nexus9.skin *
 cd ..
 
 cd iPadAir2
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPadAir2.skin *
+zip -9 ../../OTA/iPadAir2.skin *
 cd ..
                         
 cd iPadMini4
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPadMini4.skin *
+zip -9 ../../OTA/iPadMini4.skin *
 cd ..
                         
 cd iPadPro
 cp -f ../../../cn1/Themes/iOS7Theme.res .
-zip ../../iPadPro.skin *
+zip -9 ../../OTA/iPadPro.skin *
 cd ..
                         
 cd ..
-cp *.skin OTA
+
